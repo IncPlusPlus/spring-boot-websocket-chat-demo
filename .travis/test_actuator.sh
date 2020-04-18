774aba2e4da488afb9ebab958955d1cec5a23dea
Ryan Cloherty <cloherty.ryan@gmail.com>
2020-04-17 22:10:36 -0400

    Added a timeout in case things go sideways

--- .travis/test_actuator.sh
+++ .travis/test_actuator.sh
@@ -11,6 +11,11 @@ ready_regex=".*\w+ started on port\(s\):.*with context path.*"
 server_wait_sleep_time_s=10
 grep_cmd="docker logs $container_id | grep -Ex \"$ready_regex\""
 desired_actuator_status="{\"status\":\"UP\"}"
+#Time in multiples of seconds to wait for the server to start up before quitting. This * $server_wait_sleep_time_s = num seconds
+max_wait_time=6
+
+#leave this variable at zero
+time_waited=0
 
 check_actuator_status() {
   curl_result=$(curl -s "$host":"$port""$url")
@@ -34,8 +39,15 @@ while [[ "$endpoint_up" == false ]]; do
     check_actuator_status
     ;;
   1)
-    echo Server not ready. Sleeping $server_wait_sleep_time_s seconds
-    sleep $server_wait_sleep_time_s
+    if [[ "$time_waited" -gt $((server_wait_sleep_time_s * max_wait_time)) ]]; then
+      echo Timed out while waiting for server to start. Printing log from server
+      docker logs "$container_id"
+      exit 1
+    else
+      echo Server not ready. Sleeping $server_wait_sleep_time_s seconds
+      sleep $server_wait_sleep_time_s
+      time_waited=$((time_waited + server_wait_sleep_time_s))
+    fi
     ;;
   *)
     echo An error occurred while checking server status. Grep returned status "$grep_status"
@@ -43,6 +55,7 @@ while [[ "$endpoint_up" == false ]]; do
     docker logs "$container_id"
     echo Attempting grep again to print resulting error to console
     docker logs "$container_id" | grep -Ex "$ready_regex"
+    exit 1
     ;;
   esac
 done
